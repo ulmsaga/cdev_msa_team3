3e000c82d63014c566d7a192a5042d39e9f2699f
ulmsaga <sclee1115@gmail.com>
2025-02-17 17:28:38 +0900

    mod Dockerfile

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,6 @@
 prefix="group1-team3"
-app="accounts-service"
-ver="0.2.18"
+app="accounts-service-by-jenkins"
+ver="0.2.20"
 
 echo "docker build -t k8s-vga-worker1:5000/${prefix}-${app}:v${ver}"
 docker build --platform=linux/amd64 -t k8s-vga-worker1:5000/${prefix}-${app}:v${ver} ./
